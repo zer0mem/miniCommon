2835f2c99921e74d715893917d2359232e2f10ee
Peter Hlavaty <zer0mem@yahoo.com>
2014-06-29 04:54:32 +0800

    bugfixes

--- auto/MemoryObj.hpp
+++ auto/MemoryObj.hpp
@@ -3,40 +3,60 @@
 #include <sal.h>
 #include <memory>
 
-template<typename TYPE>
+template<typename Type_t>
 class CMemObj
 {
+	static
+	void
+	MemObjDisposer(
+		__inout void* objects
+		)
+	{
+		delete objects;
+	}
 public:
 	CMemObj(
 		__in size_t count
-		) : m_buff(static_cast<TYPE*>(calloc(count, sizeof(TYPE)))),
+		) : m_buff(static_cast<Type_t*>(calloc(count, sizeof(Type_t))), MemObjDisposer),
 			m_count(0)
 	{
 		if (!m_buff.get())
 			return;
+
 		m_count = count;
-		memset(m_buff.get(), 0, m_count * sizeof(TYPE));
+		memset(m_buff.get(), 0, m_count * sizeof(Type_t));
 	}
 
-	size_t GetCount()
+	~CMemObj()
+	{
+		//if not allocated at all, GetCount returns 0
+		for (size_t i = 0; i < GetCount(); i++)
+			m_buff.get()[i].~Type_t();
+	}
+
+	size_t 
+	GetCount()
 	{
 		return m_count;
 	}
 
 	__checkReturn
-	bool IsAllocated()
+	bool 
+	IsAllocated()
 	{
 		return !!GetCount();
 	}
 
 	//unsafe to use whithout IsAllocated() 
-	TYPE* operator&()
+	Type_t* 
+	operator&()
 	{
 		return m_buff.get();
 	}
 
 	//unsafe to use whithout IsAllocated() 
-	TYPE& operator[](
+	Type_t& 
+	operator[](
 		__in size_t index
 		)
 	{
@@ -45,30 +65,5 @@ public:
 
 protected:
 	size_t m_count;
-	std::unique_ptr<TYPE> m_buff;	
+	std::unique_ptr<Type_t, decltype(&MemObjDisposer)> m_buff;
 };
-
-template<typename TYPE>
-class CObjHolder :
-	public CMemObj<TYPE>
-{
-public:
-	CObjHolder(
-		__in size_t count
-		) : CMemObj(count)
-	{
-	}
-
-	~CObjHolder()
-	{
-		//if not allocated at all, GetCount returns 0
-		for (size_t i = 0; i < GetCount(); i++)
-			m_buff[i].~TYPE();
- 	}
-};
-
-/*
-typedef std::shared_ptr< CMemObj<char> > TUserBuffer;
-typedef CObjHolder<TUserModeBuffer> TSharedBuffers;
-*/
-
